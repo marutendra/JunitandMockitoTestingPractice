4ae9de46e380f9d28402d2b308d8c1f04cbe03ea
Marutendra Pratap singh <marutendrapratapsingh627@gmail.com>
2026-08-25 20:11:42 +0530

    update test

--- src/test/java/com/Junit/Student/DemoApplicationTests.java
+++ src/test/java/com/Junit/Student/DemoApplicationTests.java
@@ -150,9 +150,92 @@ void getAllStudentTest(){
         assertEquals(expectedStudent, result);
     }
 
+    // Test - 5
+
+    @Test
+    // In case when student is present
+    void deleteByIdTest1(){
+        when(studentRepository.existsById(102L)).thenReturn(true);
+        service.deleteStudent(102L);
+        verify(studentRepository).deleteById(102L);
+    }
+    @Test
+    // In case when student is not present
+    void deleteByIdTest2(){
+        when(studentRepository.existsById(102L)).thenReturn(false);
+        assertThrows(RuntimeException.class,()->service.deleteStudent(102L));
+        verify(studentRepository,never()).deleteById(102L);
+        //verify(studentRepository).deleteById(102L);
+    }
+
+    // Test-6
+    @Test
+    void existsByEmailTest(){
+        when(studentRepository.existsByEmail("abc@!23")).thenReturn(false);
+        service.existsByEmail("abc@!23");
+        verify(studentRepository).existsByEmail("abc@!23");
+    }
+   // test - 7
 
 
 
+    @Test
+     void  getStudentByCourseTest(){
+         Student studentobject1 = Student.builder()
+                 .id(102L)
+                 .name("Sunny")
+                 .age(22)
+                 .course("Java")
+                 .build();
+         Student studentobject2 = Student.builder()
+                 .id(103L)
+                 .name("Sunny")
+                 .age(22)
+                 .course("Java")
+                 .build();
+         List<Student> studentlist = List.of(studentobject1 , studentobject2);
+        when(studentRepository.findByCourse("Java")).thenReturn(studentlist);
+        List<Student> java = service.getStudentsByCourse("Java");
+        assertEquals(studentlist,java);
+        verify(studentRepository).findByCourse("Java");
+     }
 
+     // test - 8
+    @Test
+    void  calculateAverageAgeTest(){
+        Student studentobject1 = Student.builder()
+                .id(102L)
+                .name("Sunny")
+                .age(22)
+                .course("Java")
+                .build();
+        Student studentobject2 = Student.builder()
+                .id(103L)
+                .name("Sunny")
+                .age(24)
+                .course("Java")
+                .build();
+        List<Student> studentlist = List.of(studentobject1 , studentobject2);
+        when(studentRepository.findAll()).thenReturn(studentlist);
+        double v = service.calculateAverageAge();
+        assertEquals(23,v);
 
+    }
+
+    // Test - 9
+    @Test
+    void promoteStudentTest(){
+        Student student = Student.builder()
+                .id(103L)
+                .name("Sunny")
+                .age(24)
+                .marks(34.0)
+                .course("Java")
+                .build();
+        when(studentRepository.findById(103L)).thenReturn(Optional.of(student));
+        when(studentRepository.save(student)).thenReturn(student);
+        Student student1 = service.promoteStudent(103L, 23);
+        assertEquals(57,student1.getMarks());
+        verify(studentRepository ).save(student);
+    }
 }
